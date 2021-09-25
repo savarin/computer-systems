c6045911f3c94d773b38e3de464d24cc67fd181f
Ezzeri Esa <ezzeri.esa@gmail.com>
2021-09-24 22:00:43 -0700

    solution for sum_to_n

--- assembly/sum_to_n.asm
+++ assembly/sum_to_n.asm
@@ -1,4 +1,13 @@
 section .text
 global sum_to_n
-sum_to_n:
-	ret
+sum_to_n:	
+		mov		rax, 0		; set rax as running sum
+		jmp .loop
+
+.loop	test	rdi, rdi	; line up rdi for jump if zero
+		jz .end
+		add 	rax, rdi	; add rdi to rax
+		dec		rdi			; decrement rdi
+		jmp .loop
+
+.end	ret
