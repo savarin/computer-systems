d25677e18a31621262e6c151a56079964f45d6cc
Ezzeri Esa <ezzeri.esa@gmail.com>
2021-10-11 22:11:08 -0700

    convert to array

---
+++ memory-hierarchy/README.md
@@ -0,0 +1,4 @@
+
+average-age
+
+[baseline](https://github.com/savarin/computer-systems/blob/5bf88d681aa56e0f8650c00756ae18dd68a9dd0f/memory-hierarchy/metrics.go)    2207043 ns/op
--- memory-hierarchy/metrics.go
+++ memory-hierarchy/metrics.go
@@ -3,47 +3,20 @@ package metrics
 import (
 	"encoding/csv"
 	"log"
-	// "math"
 	"os"
 	"strconv"
-	"time"
 )
 
-type UserId int
-type UserMap map[UserId]*User
-
-type Address struct {
-	fullAddress string
-	zip         int
-}
-
-type DollarAmount struct {
-	dollars, cents uint64
-}
-
-type Payment struct {
-	amount DollarAmount
-	time   time.Time
-}
-
-type User struct {
-	id       UserId
-	name     string
-	age      int
-	address  Address
-	payments []Payment
-}
-
-func AverageAge(users UserMap) float64 {
+func AverageAge(ages []int) float64 {
 	average, count := 0.0, 0.0
-	for _, u := range users {
+	for _, age := range ages {
 		count += 1
-		average += (float64(u.age) - average) / count
+		average += (float64(age) - average) / count
 	}
 	return average
 }
 
-func LoadData() UserMap {
+func LoadData() []int {
 	f, err := os.Open("users.csv")
 	if err != nil {
 		log.Fatalln("Unable to read users.csv", err)
@@ -54,34 +27,10 @@ func LoadData() UserMap {
 		log.Fatalln("Unable to parse users.csv as csv", err)
 	}
 
-	users := make(UserMap, len(userLines))
-	for _, line := range userLines {
-		id, _ := strconv.Atoi(line[0])
-		name := line[1]
+	users := make([]int, len(userLines))
+	for i, line := range userLines {
 		age, _ := strconv.Atoi(line[2])
-		address := line[3]
-		zip, _ := strconv.Atoi(line[3])
-		users[UserId(id)] = &User{UserId(id), name, age, Address{address, zip}, []Payment{}}
-	}
-
-	f, err = os.Open("payments.csv")
-	if err != nil {
-		log.Fatalln("Unable to read payments.csv", err)
-	}
-	reader = csv.NewReader(f)
-	paymentLines, err := reader.ReadAll()
-	if err != nil {
-		log.Fatalln("Unable to parse payments.csv as csv", err)
-	}
-
-	for _, line := range paymentLines {
-		userId, _ := strconv.Atoi(line[2])
-		paymentCents, _ := strconv.Atoi(line[0])
-		datetime, _ := time.Parse(time.RFC3339, line[1])
-		users[UserId(userId)].payments = append(users[UserId(userId)].payments, Payment{
-			DollarAmount{uint64(paymentCents / 100), uint64(paymentCents % 100)},
-			datetime,
-		})
+		users[i] = age
 	}
 
 	return users
