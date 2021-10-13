8e799eafa3a85cf6a9acd956ff9554d8aeffbffb
Ezzeri Esa <ezzeri.esa@gmail.com>
2021-10-13 12:12:02 -0700

    run subprocess

--- exceptional-control-flow/main.go
+++ exceptional-control-flow/main.go
@@ -4,21 +4,27 @@
 package main
 
 import (
-	// "bufio"
+	"bufio"
 	"context"
 	"fmt"
+	"os"
 	"runtime"
 	"sync"
 	"time"
-	// "os"
 )
 
 var wg sync.WaitGroup
 
+func log(msg string) {
+	t := time.Now()
+	ts := fmt.Sprintf("%d-%02d-%02dT%02d:%02d:%02d", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
+	fmt.Printf("%s thread %d - %s\n", ts, runtime.NumGoroutine(), msg)
+}
+
 func main() {
+	log("main start")
 	// Rewrote as nested routines.
 	//   https://stackoverflow.com/questions/56224836/stop-child-goroutine-when-parent-returns
-	fmt.Printf("%d - main start\n", runtime.NumGoroutine())
 	wg.Add(1)
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -28,58 +34,64 @@ func main() {
 
 	// Stop main from immediate exit.
 	time.Sleep(time.Second)
-	fmt.Printf("%d - parent complete\n", runtime.NumGoroutine())
+	log("parent complete")
+	log("main end")
 }
 
 func parent(ctx context.Context) {
-	fmt.Printf("%d - parent start\n", runtime.NumGoroutine())
+	log("parent start")
 	c := make(chan int)
 
-	go child(ctx, c)
+	// NewReader returns a new Reader whose buffer has the default size.
+	//   https://pkg.go.dev/bufio#NewReader
+	reader := bufio.NewReader(os.Stdin)
 
-    select {
-    case <-ctx.Done():
-        fmt.Printf("%d - parent context expired\n", runtime.NumGoroutine())
-    case <-c:
-        fmt.Printf("%d - child complete\n", runtime.NumGoroutine())
-    case <-time.After(time.Duration(3) * time.Second):
-        fmt.Printf("%d - child timeout\n", runtime.NumGoroutine())
-    }
-    wg.Done()
+	for {
+		fmt.Print("▶️  ")
+
+		// ReadString reads until the first occurrence of delim in the input, returning a string
+		// containing the data up to and including the delimiter.
+		//   https://pkg.go.dev/bufio#Reader.ReadString
+		cmd, _ := reader.ReadString('\n')
+		cmd = cmd[:len(cmd)-1]
+
+		if cmd == "exit" {
+			fmt.Println("Exit shell.")
+			break
+		}
+
+		if cmd == "pwd" || cmd == "ls" {
+			go child(ctx, cmd, c)
+
+			select {
+			case <-ctx.Done():
+				log("parent content expired")
+			case <-c:
+				log("child complete")
+			case <-time.After(time.Duration(3) * time.Second):
+				log("child timeout")
+			}
+
+			continue
+		}
+
+		fmt.Printf("%s: command not found\n", cmd)
+	}
+
+	log("parent end")
+	wg.Done()
 }
 
-func child(ctx context.Context, c chan int) {
-	fmt.Printf("%d - child start\n", runtime.NumGoroutine())
-    select {
-    case <-ctx.Done():
-        fmt.Printf("%d - child context expired\n", runtime.NumGoroutine())
-    // Long-running task as a sleep.
-    case <-time.After(time.Duration(1) * time.Second):
-        fmt.Printf("%d - child done\n", runtime.NumGoroutine())
-    }
+func child(ctx context.Context, cmd string, c chan int) {
+	log("child start")
 
-    fmt.Printf("%d - child end\n", runtime.NumGoroutine())
-    c <- 1
+	switch cmd {
+	case "pwd":
+		fmt.Println("present working directory")
+	case "ls":
+		fmt.Println("list directory contents")
+	}
+
+	log("child end")
+	c <- 1
 }
-
-
-// 	// NewReader returns a new Reader whose buffer has the default size.
-// 	//   https://pkg.go.dev/bufio#NewReader
-// 	reader := bufio.NewReader(os.Stdin)
-
-// 	for {
-// 		fmt.Print("▶️  ")
-
-// 		// ReadString reads until the first occurrence of delim in the input, returning a string
-// 		// containing the data up to and including the delimiter.
-// 		//   https://pkg.go.dev/bufio#Reader.ReadString
-// 		input, _ := reader.ReadString('\n')
-
-// 		if input == "exit\n" {
-// 			fmt.Println("Exit shell.")
-// 			break
-// 		}
-
-// 		fmt.Println(input)
-// 	}
-// }
